ed0d56473ad620b7c6f0261aee718e7781cbed9e
Vinh Nguyen <vinh.nguyen@codestringers.com>
2021-03-06 12:11:37 +0700

    add config

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require('express-validator/check');
+const gravatar = require('gravatar');
+const { check, validationResult } = require('express-validator');
 
 // @route  POST api/users
 // @Desc Register route
@@ -9,12 +10,42 @@ router.post('/', [
     check('name', 'Name is required').not().isEmpty(),
     check('email', 'Please included a valid email').isEmail(),
     check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
-], (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() })
-    }
-    res.send('User route');
-});
+],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() })
+        }
+
+        const { name, email, password } = req.body;
+
+        try {
+            let user = await User.findOne({ email: email });
+
+            // See if user exists
+            if (user) {
+                res.status(400).json({ error: [{ msg: " User already exists " }] });
+            }
+
+            // Get user  gravatar
+            const avatar = gravatar.url(email, {
+                s: '200', // default size
+                r: 'pg',
+                d: 'mm'
+            })
+
+            user = new User
+            // Encrypt password
+
+            // Return jsonwebtoken
+
+            res.send('User route');
+
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server error');
+        }
+
+    });
 
 module.exports = router;
